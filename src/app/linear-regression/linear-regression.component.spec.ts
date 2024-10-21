ba597bf47e9a4ecc6d522f6cf6ec8305487f4dc3
feriveramar <fernandaa.riveramartinez@gmail.com>
2024-10-21 14:12:53 -0600

    add update linear-regression

--- src/app/linear-regression/linear-regression.component.spec.ts
+++ src/app/linear-regression/linear-regression.component.spec.ts
@@ -3,7 +3,6 @@ import { LinearRegressionComponent } from './linear-regression.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ReadFileService } from '../classes/readFile';
 
-
 describe('LinearRegressionComponent', () => {
   let component: LinearRegressionComponent;
   let fixture: ComponentFixture<LinearRegressionComponent>;
@@ -25,66 +24,65 @@ describe('LinearRegressionComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('Should return b0 approximately -22.55 with the dataset Data_Test1', async () => {
+  it('Should return b0 approximately -22.55 with the dataset x.txt and y2.txt', async () => {
     const result = await component.b0('assets/x.txt', 'assets/y2.txt');
     expect(result).toBeCloseTo(-22.55, 2);
   });
 
-  it('Should return b1 approximately 1.7279 with the dataset Data_Test1', async () => {
+  it('Should return b1 approximately 1.7279 with the dataset x.txt and y2.txt', async () => {
     const result = await component.b1('assets/x.txt', 'assets/y2.txt');
     expect(result).toBeCloseTo(1.7279, 2);
   });
 
-  it('Should return yk approximately 644.429 with the dataset Data_Test1 if x=386', async () => {
+  it('Should return yk approximately 644.429 with the dataset x.txt and y2.txt if x=386', async () => {
     const result = await component.yk('assets/x.txt', 'assets/y2.txt');
     expect(result).toBeCloseTo(644.429, 2);
   });
 
-  it('Should return b0 approximately -4.039  with the dataset Data_Test2', async () => {
+  it('Should return b0 approximately -4.039  with the dataset x.txt and y3.txt', async () => {
     const result = await component.b0('assets/x.txt', 'assets/y3.txt');
     expect(result).toBeCloseTo(-4.039 , 2);
   });
 
-  it('Should return b1 approximately 0.1681 with the dataset Data_Test2', async () => {
+  it('Should return b1 approximately 0.1681 with the dataset x.txt and y3.txt', async () => {
     const result = await component.b1('assets/x.txt', 'assets/y3.txt');
     expect(result).toBeCloseTo(0.1681, 2);
   });
 
-  it('Should return yk approximately 60.858 with the dataset Data_Test2 if x=386', async () => {
-    const result = await component.yk('assets/x.txt', 'assets/y3.txt');
+  it('Should return yk approximately 60.858 with the dataset x.txt and y3.txt if x=386', async () => {
+    const result = await component.yk('assets/x.txt', 'assets/y3.txt');  
     expect(result).toBeCloseTo(60.858, 2);
-  });
+  }); 
 
-  it('Should return b0 approximately -23.92 with the dataset Data_Test3', async () => {
+  it('Should return b0 approximately -23.92 with the dataset y1.txt and y2.txt', async () => {
     const result = await component.b0('assets/y1.txt', 'assets/y2.txt');
     expect(result).toBeCloseTo(-23.92, 2);
   });
 
-  it('Should return b1 approximately 1.43097 with the dataset Data_Test3', async () => {
+  it('Should return b1 approximately 1.43097 with the dataset y1.txt and y2.txt', async () => {
     const result = await component.b1('assets/y1.txt', 'assets/y2.txt');
     expect(result).toBeCloseTo(1.43097, 2);
   });
 
-  it('Should return yk approximately 528.4294 with the dataset Data_Test3 if x=386', async () => {
+  it('Should return yk approximately 528.4294 with the dataset y1.txt and y2.txt if x=386', async () => {
     const result = await component.yk('assets/y1.txt', 'assets/y2.txt');
     expect(result).toBeCloseTo(528.4294, 2);
   });
 
   
-  it('Should return b0 approximately -4.604 with the dataset Data_Test4', async () => {
+  it('Should return b0 approximately -4.604 with the dataset y1.txt and y3.txt', async () => {
     const result = await component.b0('assets/y1.txt', 'assets/y3.txt');
     expect(result).toBeCloseTo(-4.604, 2);
   });
 
-  it('Should return b1 approximately 0.16064 with the dataset Data_Test4', async () => {
+  it('Should return b1 approximately 0.16064 with the dataset y1.txt and y3.txt', async () => {
     const result = await component.b1('assets/y1.txt', 'assets/y3.txt');
     expect(result).toBeCloseTo(0.140164
       , 2);
   });
 
-  it('Should return yk approximately 49.4994 with the dataset Data_Test4 if x=386', async () => {
+  it('Should return yk approximately 49.4994 with the dataset y1.txt and y3.txt if x=386', async () => {
     const result = await component.yk('assets/y1.txt', 'assets/y3.txt');
-    //const result = await component.yk('assets/y1.txt', 'assets/y3.txt');
     expect(result).toBeCloseTo(49.4994, 2);
   });
 
